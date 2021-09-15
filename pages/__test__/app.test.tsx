366d1b17f00a711495e57aa2b5a7778529daa19e
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2021-09-15 22:42:17 +0200

    Remove testing config

--- pages/__test__/app.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from 'test-utils'
-import App from '../_app'
-import Home from '../'
-
-test('renders a message', () => {
-	render(<App Component={Home} pageProps={undefined} router={undefined} />)
-	screen.getByText('HOLA')
-})
