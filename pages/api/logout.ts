5b15a0e2163a6d16f892d2aae04c0d41edf50fd2
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2021-09-16 21:11:51 +0200

    Return error

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -7,8 +7,8 @@ initAuth()
 const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
 	try {
 		await unsetAuthCookies(req, res)
-	} catch (e) {
-		return res.status(500).json({ error: 'Unexpected error.' })
+	} catch (error) {
+		return res.status(500).json({ error })
 	}
 	return res.status(200).json({ success: true })
 }
